8e683797b9799b370fcd7942ba670e5579ef3fa1
Victor Orozco <me@vorozco.com>
2025-10-02 18:28:25 -0600

    I am a good developer

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,12 +11,10 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        String deadText = doSpotBugsDemo();
         return "Hello from Quarkus REST";
     }
 
     private String doSpotBugsDemo(){
-        String deadText = "This is a dead text as well";
         return "SpotBugs demo";
     }
 }
